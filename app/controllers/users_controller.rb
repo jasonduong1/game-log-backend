03ccd230bd586bf6b34e7afe96a183acec604191
jason <jduong89@gmail.com>
2022-04-15 13:33:19 -0700

    adds user create action/route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(
+      name: params[:name],
+      user_name: params[:user_name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
 end
