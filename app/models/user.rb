6b3c6f2d91710df0e348fb35a146aaee4ec82e04
jason <jduong89@gmail.com>
2022-04-15 13:30:12 -0700

    adds user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :user_name, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :name, presence: true
 end
