ab9c031115ab67dc64b36b4e1a05e98bd9f6a3b6
jason <jduong89@gmail.com>
2022-04-21 10:28:53 -0700

    adds game show/index actions/path

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,11 @@
 class GamesController < ApplicationController
+  def index
+    response = HTTP.get("https://api.rawg.io/api/games?key=#{Rails.application.credentials.rawg.api_key}")
+    render json: response.parse(:json)
+  end
+
+  def show
+    response = HTTP.get("https://api.rawg.io/api/games/#{params[:id]}?key=#{Rails.application.credentials.rawg.api_key}")
+    render json: response.parse(:json)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
+  get "/games" => "games#index"
+  get "/games/:id" => "games#show"
 end
