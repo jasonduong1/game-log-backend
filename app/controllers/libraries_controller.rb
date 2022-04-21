1de2d4e5d3e054f2e63e11ea0552bae2087a2501
jason <jduong89@gmail.com>
2022-04-21 12:02:03 -0700

    adds create/update/delete action/path

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -3,8 +3,43 @@ class LibrariesController < ApplicationController
     libraries = Library.all
     render json: libraries
   end
+
   def show
     library = Library.find_by(id: params[:id])
     render json: library
+  end
+
+  def create
+    library = Library.new(
+      user_id: current_user.id,
+      game_id: params[:game_id],
+      rating: params[:rating],
+      review: params[:review],
+      note: params[:note],
+    )
+    if library.save
+      render json: library
+    else
+      render json: { errors: library.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def update
+    library = Library.find_by(id: params[:id])
+    library.game_id = params[:game_id] || library.game_id
+    library.rating = params[:rating] || library.rating
+    library.review = params[:review] || library.review
+    library.note = params[:note] || library.note
+    if library.save
+      render json: library
+    else
+      render json: { errors: library.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def destroy
+    library = Library.find_by(id: params[:id])
+    library.destroy
+    render json: { message: "Game removed from library!" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
+
   get "/games" => "games#index"
   get "/games/:id" => "games#show"
+
   get "/libraries" => "libraries#index"
   get "/libraries/:id" => "libraries#show"
+  post "/libraries" => "libraries#create"
+  patch "/libraries/:id" => "libraries#update"
+  delete "/libraries/:id" => "libraries#destroy"
 end
