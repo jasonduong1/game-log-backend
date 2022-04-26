80af5922c6af3d7d73932989941d878b4c1d4d5b
jason <jduong89@gmail.com>
2022-04-25 22:23:36 -0700

    minor fixes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,13 @@
 class GamesController < ApplicationController
   def index
-    response = HTTP.get("https://api.rawg.io/api/games?key=#{Rails.application.credentials.rawg.api_key}&page_size=40")
+    response = HTTP.get("https://api.rawg.io/api/games?key=#{Rails.application.credentials.rawg.api_key}")
     render json: response.parse(:json)
   end
 
   def show
     response = HTTP.get("https://api.rawg.io/api/games/#{params[:id]}?key=#{Rails.application.credentials.rawg.api_key}")
     render json: response.parse(:json)
+  end
 
   def search
     response = HTTP.get("https://api.rawg.io/api/games?key=#{Rails.application.credentials.rawg.api_key}&#{params[:search]}")
