418866765a5cdd8581c34203656f02fdd1d093f9
jason <jduong89@gmail.com>
2022-04-26 21:00:18 -0700

    adds if condition in index for search param

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,16 +1,15 @@
 class GamesController < ApplicationController
   def index
     response = HTTP.get("https://api.rawg.io/api/games?key=#{Rails.application.credentials.rawg.api_key}")
+
+    if params[:search]
+      response = HTTP.get("https://api.rawg.io/api/games?key=#{Rails.application.credentials.rawg.api_key}&search=#{params[:search]}")
+    end
     render json: response.parse(:json)
   end
 
   def show
     response = HTTP.get("https://api.rawg.io/api/games/#{params[:id]}?key=#{Rails.application.credentials.rawg.api_key}")
-    render json: response.parse(:json)
-  end
-
-  def search
-    response = HTTP.get("https://api.rawg.io/api/games?key=#{Rails.application.credentials.rawg.api_key}&#{params[:search]}")
     render json: response.parse(:json)
   end
 end
