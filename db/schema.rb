6fd5a217ca61fc2535d22442bff00b9429ee0c78
jason <jduong89@gmail.com>
2022-04-29 13:31:24 -0700

    migrates changes to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_29_193932) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_29_194106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "games", force: :cascade do |t|
     t.string "name"
-    t.string "released"
-    t.string "background_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "api_id"
+    t.string "image"
+    t.integer "rating"
+    t.text "review"
+    t.text "note"
   end
 
   create_table "libraries", force: :cascade do |t|
     t.integer "user_id"
     t.integer "game_id"
-    t.integer "rating"
-    t.text "review"
-    t.text "note"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
