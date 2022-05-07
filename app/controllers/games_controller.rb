977e80e945eb38acba6ba07d5690cca35ae95f75
jason <jduong89@gmail.com>
2022-05-07 13:46:27 -0700

    adds 24hr cache for games show/index

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,11 +5,13 @@ class GamesController < ApplicationController
     if params[:search]
       response = HTTP.get("https://api.rawg.io/api/games?key=#{Rails.application.credentials.rawg.api_key}&search=#{params[:search]}")
     end
+    expires_in 24.hours
     render json: response.parse(:json)
   end
 
   def show
     response = HTTP.get("https://api.rawg.io/api/games/#{params[:id]}?key=#{Rails.application.credentials.rawg.api_key}")
+    expires_in 24.hours
     render json: response.parse(:json)
   end
 end
