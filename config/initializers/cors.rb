7e25b07489f0337e3085f8bb2c97e118bbede784
jason <jduong89@gmail.com>
2022-05-16 12:24:56 -0700

    adds link to netlify frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "localhost:8080", "curious-trifle-5e3396.netlify.app"
 
     resource "*",
       headers: :any,
