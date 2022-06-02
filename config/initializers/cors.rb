c43aa48d7170d5ae7c339b644c30a32555efc151
jason <jduong89@gmail.com>
2022-06-01 20:59:21 -0700

    edits cors to allow frontend access

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "curious-trifle-5e3396.netlify.app"
+    origins "localhost:8080", "game-log.netlify.app"
 
     resource "*",
       headers: :any,
